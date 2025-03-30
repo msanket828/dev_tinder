ffb867aba431d45746a1d1073a70491580514cc6
msanket828 <sammane228@gmail.com>
2025-03-30 15:29:06 +0530

    multiple route handler

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.use("/user", (req, res) => {
-  console.log(req.query);
-  res.send("user data...");
-});
-
-app.use("/user/:name/:salary", (req, res) => {
-  console.log(req.params);
-  res.send("user data... ");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("inside handler 1");
+    // res.send("1st response...");
+    next();
+  },
+  (req, res, next) => {
+    console.log("inside handler 2");
+    // res.send("2nd response...");
+    next();
+  },
+  (req, res, next) => {
+    console.log("insdie handler 3");
+    next();
+    // res.send("3rd response...");
+  },
+  (req, res, next) => {
+    console.log("inside handler 4");
+    next();
+    res.send("4th response...");
+  }
+);
 
 app.listen(PORT, () => {
   console.log("server in running on port", PORT);
