34ca574cd3de2637001e7d96b09e5a0e635fd0b3
msanket828 <sammane228@gmail.com>
2025-05-12 16:46:52 +0530

    added pagination in feed

--- src/routers/user.js
+++ src/routers/user.js
@@ -59,6 +59,11 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId toUserId");
@@ -78,7 +83,10 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
           _id: { $ne: loggedInUser._id },
         },
       ],
-    });
+    })
+      .select(USER_DATA)
+      .skip(skip)
+      .limit(limit);
 
     res.send({
       records: users.length,
